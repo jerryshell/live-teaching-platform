a094ec341b85c50945d277c7002f58be83c67e28
Jerry <calljerryli@outlook.com>
2019-04-16 16:16:51 +0800

    课程回放列表连接数据库

--- src/main/java/cn/jerryshell/liveteaching/controller/VideoController.java
+++ src/main/java/cn/jerryshell/liveteaching/controller/VideoController.java
@@ -1,6 +1,9 @@
 package cn.jerryshell.liveteaching.controller;
 
 import cn.jerryshell.liveteaching.config.UploadVideoConfig;
+import cn.jerryshell.liveteaching.dao.CourseDao;
+import cn.jerryshell.liveteaching.dao.VideoDao;
+import cn.jerryshell.liveteaching.model.Course;
 import cn.jerryshell.liveteaching.model.Video;
 import cn.jerryshell.liveteaching.service.VideoService;
 import org.slf4j.Logger;
@@ -11,21 +14,27 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.net.MalformedURLException;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/video")
 public class VideoController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     private VideoService videoService;
     private UploadVideoConfig uploadVideoConfig;
+    private VideoDao videoDao;
+    private CourseDao courseDao;
 
     @Autowired
     public void setVideoService(VideoService videoService) {
@@ -37,12 +46,35 @@ public class VideoController {
         this.uploadVideoConfig = uploadVideoConfig;
     }
 
-    @GetMapping
-    public String index() {
+    @Autowired
+    public void setVideoDao(VideoDao videoDao) {
+        this.videoDao = videoDao;
+    }
+
+    @Autowired
+    public void setCourseDao(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
+    @GetMapping("/video")
+    public String list(Model model) {
+        List<Course> courseList = courseDao.findAll();
+        model.addAttribute("courseList", courseList);
+        List<Video> videoList = videoDao.findAll();
+        model.addAttribute("videoList", videoList);
         return "video-list";
     }
 
-    @PostMapping("/upload")
+    @GetMapping("/video/course/{courseId}")
+    public String listByCourseId(@PathVariable String courseId, Model model) {
+        List<Course> courseList = courseDao.findAll();
+        model.addAttribute("courseList", courseList);
+        List<Video> videoList = videoDao.findByCourseId(courseId);
+        model.addAttribute("videoList", videoList);
+        return "video-list";
+    }
+
+    @PostMapping("/video/upload")
     public String upload(Video video,
                          @RequestParam("uploadFile") MultipartFile uploadFile,
                          HttpSession session) {
@@ -56,20 +88,20 @@ public class VideoController {
         return "redirect:/user";
     }
 
-    @GetMapping("/{videoName}")
+    @GetMapping("/video/{videoName}")
     public ModelAndView video(@PathVariable String videoName) {
         ModelAndView modelAndView = new ModelAndView("video.html");
         modelAndView.addObject("videoName", videoName);
         return modelAndView;
     }
 
-    @GetMapping("/delete/{videoName}")
+    @GetMapping("/video/delete/{videoName}")
     public String deleteVideo(@PathVariable String videoName) {
         videoService.deleteVideo(videoName);
         return "redirect:/user";
     }
 
-    @GetMapping("/download/{filename}")
+    @GetMapping("/video/download/{filename}")
     public ResponseEntity<Resource> download(@PathVariable String filename) throws MalformedURLException {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
--- src/main/java/cn/jerryshell/liveteaching/dao/VideoDao.java
+++ src/main/java/cn/jerryshell/liveteaching/dao/VideoDao.java
@@ -4,6 +4,9 @@ import cn.jerryshell.liveteaching.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideoDao extends JpaRepository<Video, String> {
+    List<Video> findByCourseId(String courseId);
 }
