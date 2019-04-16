94eaafa719bffc626bf96659abb1d7869eb93af0
Jerry <calljerryli@outlook.com>
2019-04-16 14:54:36 +0800

    注册连接数据库

--- src/main/java/cn/jerryshell/liveteaching/controller/RegisterController.java
+++ src/main/java/cn/jerryshell/liveteaching/controller/RegisterController.java
@@ -1,7 +1,12 @@
 package cn.jerryshell.liveteaching.controller;
 
+import cn.jerryshell.liveteaching.dao.StudentDao;
+import cn.jerryshell.liveteaching.dao.TeacherDao;
 import cn.jerryshell.liveteaching.model.Student;
 import cn.jerryshell.liveteaching.model.Teacher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +15,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class RegisterController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private StudentDao studentDao;
+    private TeacherDao teacherDao;
+
+    @Autowired
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    @Autowired
+    public void setTeacherDao(TeacherDao teacherDao) {
+        this.teacherDao = teacherDao;
+    }
+
     @GetMapping("/register/{kind}")
     public String toRegisterPage(@PathVariable String kind) {
         return "register-" + kind;
@@ -17,13 +36,23 @@ public class RegisterController {
 
     @PostMapping("/register/student")
     public String doRegisterStudent(Student student) {
-        System.out.println(student);
+        logger.info(student.toString());
+        Student studentFromDB = studentDao.findById(student.getId()).orElse(null);
+        if (studentFromDB != null) {
+            return "redirect:/register/student";
+        }
+        studentDao.save(student);
         return "redirect:/login";
     }
 
     @PostMapping("/register/teacher")
     public String doRegisterTeacher(Teacher teacher) {
-        System.out.println(teacher);
+        logger.info(teacher.toString());
+        Teacher teacherFromDB = teacherDao.findById(teacher.getId()).orElse(null);
+        if (teacherFromDB != null) {
+            return "redirect:/register/teacher";
+        }
+        teacherDao.save(teacher);
         return "redirect:/login";
     }
 }
