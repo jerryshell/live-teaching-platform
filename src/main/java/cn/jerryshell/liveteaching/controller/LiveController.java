957c45817c5fe7fb1d69e52f0d5ac6e3b9130501
Jerry <calljerryli@outlook.com>
2019-04-17 13:40:21 +0800

    结束直播连接数据库

--- src/main/java/cn/jerryshell/liveteaching/controller/LiveController.java
+++ src/main/java/cn/jerryshell/liveteaching/controller/LiveController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,6 +88,12 @@ public class LiveController {
         live.setTeacherId(session.getAttribute("loginUserId").toString());
         logger.info(live.toString());
         liveDao.save(live);
+        return "redirect:/user";
+    }
+
+    @DeleteMapping("/live/{id}")
+    public String deleteLiveById(@PathVariable String id) {
+        liveDao.deleteById(id);
         return "redirect:/user";
     }
 }
